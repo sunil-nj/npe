16b8978416ea3909ea7f2707aaccd739fb707231
Sunil <sunil@Sunils-MacBook-Air.local>
2022-10-18 15:28:03 +0530

    added systemGenerate_id

--- src/components/services/PaymentAPI.js
+++ src/components/services/PaymentAPI.js
@@ -3,9 +3,10 @@
 export function callPaymentAPI(payload) {
 
     return fetch(
-        `http://npeprocessnew-env.eba-3mdivn3b.us-east-1.elasticbeanstalk.com/payment`,
+        `https://npeprocessnew-env.eba-3mdivn3b.us-east-1.elasticbeanstalk.com/payment`,
         {
             method: "POST",
+            mode: 'no-cors',
             headers: {
                 "Content-Type": "application/json",
             },
